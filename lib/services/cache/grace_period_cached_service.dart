9c47c22789e446e9340ca66dbd2daeac6d35ae25
hieu.ho <hieu.ale.it@gmail.com>
2023-02-10 10:25:45 +0700

    add grace period

--- lib/services/cache/grace_period_cached_service.dart
+++ lib/services/cache/grace_period_cached_service.dart
@@ -1,6 +1,7 @@
-import 'package:iWarden/controllers/vehicle_information_controller.dart';
 import 'package:iWarden/models/vehicle_information.dart';
-
+import '../../controllers/index.dart';
+import '../../models/pagination.dart';
+import '../local/created_vehicle_data_local_service.dart';
 import 'cache_service.dart';
 
 class GracePeriodCachedService extends CacheService<VehicleInformation> {
@@ -9,14 +10,30 @@ class GracePeriodCachedService extends CacheService<VehicleInformation> {
     zoneId = initZoneId;
   }
 
-  @override
+   @override
   syncFromServer() async {
-    var paging = await vehicleInfoController.getVehicleInfoList(
-        vehicleInfoType: VehicleInformationType.GRACE_PERIOD.index,
-        zoneId: zoneId,
-        page: 1,
-        pageSize: 1000);
+    var paging = await weakNetworkVehicleInfoController
+        .getVehicleInfoList(
+            vehicleInfoType: VehicleInformationType.GRACE_PERIOD.index,
+            zoneId: zoneId,
+            page: 1,
+            pageSize: 1000)
+        .catchError((err) async {
+      var vehicleInfos = await getAll();
+      return Pagination(
+          page: 0,
+          pageSize: 1000,
+          total: vehicleInfos.length,
+          totalPages: 1,
+          rows: vehicleInfos);
+    });
     var vehicleInfos = paging.rows as List<VehicleInformation>;
     set(vehicleInfos);
+  }
+
+  getAllWithCreatedOnTheOffline() async {
+    var cachedItems = await getAll();
+    var issuedItem = await createdVehicleDataLocalService.getAllGracePeriod();
+    return cachedItems.addAll(issuedItem);
   }
 }
