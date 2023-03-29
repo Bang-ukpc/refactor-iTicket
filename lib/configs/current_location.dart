99b5c566035fe388007337fdecbf91e8fa277e3e
bang-ukpc <bang.nguyen@ukparkingcontrol.com>
2023-03-29 05:06:55 +0000

    update permission location status when user accept permission

--- lib/configs/current_location.dart
+++ lib/configs/current_location.dart
@@ -1,7 +1,9 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:iWarden/helpers/logger.dart';
 
 class CurrentLocation {
   Position? currentLocation;
+  Logger logger = Logger<CurrentLocation>();
 
   LocationSettings locationSettings = AndroidSettings(
     accuracy: LocationAccuracy.high,
@@ -17,12 +19,16 @@ class CurrentLocation {
     }
 
     if (currentLocation != null) {
+      logger.info('current position is not null');
       Geolocator.getPositionStream(locationSettings: locationSettings)
           .listen((Position? location) async {
         currentLocation = location;
       });
     } else {
+      logger.info('current position is null');
       currentLocation = await Geolocator.getCurrentPosition();
+      logger.info("get current position failed");
+      currentLocation ??= await Geolocator.getLastKnownPosition();
     }
 
     return currentLocation;
