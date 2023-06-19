d3152c83f1a92a473ec9ff0c16baae5bb5889a55
bang-ukpc <bang.nguyen@ukparkingcontrol.com>
2023-06-19 13:09:00 +0100

    WRDN-804 PO Funmi state the app didn't recognize that she was on site and NOT allow her to log in.

--- lib/screens/location/location_screen.dart
+++ lib/screens/location/location_screen.dart
@@ -200,7 +200,6 @@ class _LocationScreenState extends BaseStatefulState<LocationScreen> {
       }
     }
 
-    print("GPS ${listFilter.isNotEmpty ? listFilter[0] : "null"}");
     Future<void> showMyDialog() async {
       return showDialog<void>(
         context: context,
@@ -228,7 +227,9 @@ class _LocationScreenState extends BaseStatefulState<LocationScreen> {
                   style: CustomTextStyle.h5.copyWith(
                     color: Colors.white,
                   )),
-              onPressed: () {
+              onPressed: () async {
+                await currentLocationPosition.getCurrentLocation();
+                if (!mounted) return;
                 Navigator.of(context).pop();
               },
             ),
