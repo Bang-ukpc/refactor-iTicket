f62d9abc852774f4662d1ced304c21e4ccdbb8be
bang-ukpc <bang.nguyen@ukparkingcontrol.com>
2023-11-14 22:27:50 +0700

    first commit

---
+++ build/flutter_kronos/generated/source/buildConfig/release/com/stark/flutter_kronos/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.stark.flutter_kronos;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.stark.flutter_kronos";
+  public static final String BUILD_TYPE = "release";
+}
